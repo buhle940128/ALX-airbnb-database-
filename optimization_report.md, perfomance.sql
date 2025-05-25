8c083be5212616a8f813a131aadde8ae02346604
buhle janda <jandabuhle28@gmail.com>
2025-05-25 20:26:54 +0200

    Update optimization_report.md, perfomance.sql

--- optimization_report.md, perfomance.sql
+++ optimization_report.md, perfomance.sql
@@ -3,4 +3,6 @@ booking id:primary key,UUID,indexed
 start date:DATE,NOT NULL
 end date:DATE,NOT NULL
 status:ENUM(pending,comfirmed,canceled),NOT NULL
-
+User id:primary key,uuid,indexed
+first name:VARCHAR,NOT NULL
+email:VARCHAR,UNIQUE,NOT NULL
